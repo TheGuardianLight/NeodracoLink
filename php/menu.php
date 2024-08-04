d7bd046e69e401acd8bd20afc3299f19bccf1a37
Noa Ledet <noa.ledet@proton.me>
2024-08-04 20:23:45 +0200

    Commit

--- php/menu.php
+++ php/menu.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Mon Hub</a>
+        <a class="navbar-brand" href="index.php">Neodraco's Link</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -11,7 +11,7 @@
                         Gestion
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="sites-categories.php">Sites & Réseaux</a></li>
+                        <li><a class="dropdown-item" href="modify.php">Sites & Réseaux</a></li>
                         <li><a class="dropdown-item" href="data.php">Export/Import data</a></li>
                     </ul>
                 </li>
