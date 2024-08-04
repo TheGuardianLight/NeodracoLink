7aaae3e08de9561e72b76dd67438166f8a45c398
Noa Ledet <noa.ledet@proton.me>
2024-08-05 00:04:14 +0200

    Change

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 /*
  * Copyright (c) 2024 - Veivneorul. This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License (BY-NC-ND 4.0).
  */
+global $dbConfig;
 session_start();
 
 if (!isset($_SESSION['username'])) {
