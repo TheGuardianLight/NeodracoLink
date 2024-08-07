c47b77cc6cac8b97814adb8e89e3b0e25f4a5f1f
Noa Ledet <noa.ledet@proton.me>
2024-08-07 15:30:52 +0200

    Ajout photo de profil.

--- php/menu.php
+++ php/menu.php
@@ -3,7 +3,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">
-            <img src="/images/logo/logo-transparent-png-court.png" alt="" width="30" class="d-inline-block align-text-top">
             Neodraco's Link
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
