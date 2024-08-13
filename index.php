2aeebe92bd044ed9b7b31b8afec51ce8db736ada
Noa Ledet <noa.ledet@proton.me>
2024-08-13 21:17:39 +0200

    Modification pour n'authoriser que certain extension de fichier pour l'image de profil.

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $sites = $query->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>Mon Hub</title>
+    <title>Neodraco's Link</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <link href="styles.css" rel="stylesheet"/>
