d589f62621244ed0430eb5fdb0398542c737e4ab
Noa Ledet <noa.ledet@proton.me>
2024-09-02 14:12:36 +0200

    Ajout d'un lien vers la documentation.

--- php/menu.php
+++ php/menu.php
@@ -49,6 +49,9 @@
                 <li class="nav-item align-self-center">
                     <a class="nav-link" href="about.php">A propos</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="doc/" disabled="">Documentation</a>
+                </li>
             </ul>
         </div>
     </div>
